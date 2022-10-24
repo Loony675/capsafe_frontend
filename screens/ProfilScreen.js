4eb59735829d16b223c2a82cf8163f907d1f366d
Ferrieres <benoitferrieres1@gmail.com>
2022-10-25 01:09:09 +0200

    modif profilscreen

--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -33,53 +33,51 @@ useEffect(() => {
   <View style = {styles.mainContainer}>
     <Image style = {styles.logo} source={require("../assets/imageProfil.png")}/>
     <View style = {styles.MainInfoContainer}>
-
-
-  <View style ={styles.infoContainer}>
-    <Text>Prénom:</Text>
-    <Text>{profilInfos.firstName}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Nom:</Text>
-    <Text>{profilInfos.lastName}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Email:</Text>
-    <Text>{profilInfos.email}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Date de Naissance:</Text>
-    <Text>{profilInfos.birthdayDate}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Sexe:</Text>
-    <Text>{profilInfos.sexe}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Ligne Préférée:</Text>
-    <Text>{profilInfos.favoriteTransportLine}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Voyager avec des personnes du même sexe:</Text>
-    <Text>{profilInfos.travelingWithSameSex}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Afficher mon sexe sur le profil:</Text>
-    <Text>{profilInfos.showSexOnProfil}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Date d'enregistrement:</Text>
-    <Text>{profilInfos.registerDate}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Délai avant notification d'urgence:</Text>
-    <Text>{profilInfos.emergencyTime}</Text>
-  </View>
-  <View style ={styles.infoContainer}>
-    <Text>Note:</Text>
-    <Text>{profilInfos.score}</Text>
-  </View>
-  </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Prénom:</Text>
+        <Text style ={styles.text2}>{profilInfos.firstName}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Nom:</Text>
+        <Text style ={styles.text2}>{profilInfos.lastName}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Email:</Text>
+        <Text style ={styles.text2}>{profilInfos.email}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Date de Naissance:</Text>
+        <Text style ={styles.text2}>{profilInfos.birthdayDate}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Sexe:</Text>
+        <Text style ={styles.text2}>{profilInfos.sexe}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Ligne Préférée:</Text>
+        <Text style ={styles.text2}>{profilInfos.favoriteTransportLine}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Voyager avec des personnes du même sexe:</Text>
+        <Text style ={styles.text2}>{profilInfos.travelingWithSameSex}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Afficher mon sexe sur le profil:</Text>
+        <Text style ={styles.text2}>{profilInfos.showSexOnProfil}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Date d'enregistrement:</Text>
+        <Text style ={styles.text2}>{profilInfos.registerDate}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Délai avant notification d'urgence:</Text>
+        <Text style ={styles.text2}>{profilInfos.emergencyTime}</Text>
+      </View>
+      <View style ={styles.infoContainer}>
+        <Text style ={styles.text1}>Note:</Text>
+        <Text style ={styles.text2}>{profilInfos.score}</Text>
+      </View>
+    </View>
   </View>
   )
 }
@@ -94,24 +92,38 @@ const styles = StyleSheet.create({
     justifyContent : "center",
   },
   logo: {
-    marginTop: 10,
+    marginTop: 100,
     height:100,
     width: 100,
-    borderRadius:9999
+    borderRadius: 20
   },
   MainInfoContainer: {
-    height: "70%",
-    width: "75%",
+    height: "68%",
+    width: "80%",
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: "5%",
-    backgroundColor: "rgba(	124, 96, 183, 0.1)",
+    justifyContent: "space",
+    marginTop: "15%",
+    backgroundColor: "white",
+    borderRadius: 20
   },
+
   infoContainer: {
     // height: 30,
     width: "100%",
-    alignItems: "center",
+    alignItems: "left",
     justifyContent: "center",
     backgroundColor: "rgba(	124, 96, 183, 0.1)",
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 18
+  },
+
+  text1:{
+    fontSize: 15,
+  },
+
+  text2:{
+    color: "#478bbc",
+    fontSize: 15
   },
 })
