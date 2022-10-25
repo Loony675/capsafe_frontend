a616928ecc238df3bc9297dbcac72988d689810f
Loussot <theo.loussot@gmail.com>
2022-10-25 12:38:50 +0200

    bottom sheet

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,39 +3,44 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import MapView, {Marker} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+
+// Botton Sheet
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function MainScreen({ navigation }) {
- 
-  const [currentPosition, setCurrentPosition] = useState(null)
+  const [currentPosition, setCurrentPosition] = useState(null);
 
   // Demande accord géolocation
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
-      console.log('Status', status);
-      if (status === 'granted') {
-        Location.watchPositionAsync({ distanceInterval: 10 },
-          (location) => {
-            console.log('Location--->', location);
-            setCurrentPosition(location.coords);
-          });
+      console.log("Status", status);
+      if (status === "granted") {
+        Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+          console.log("Location--->", location);
+          setCurrentPosition(location.coords);
+        });
       }
     })();
-
   }, []);
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <MapView style={styles.map}>
-        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
+        {currentPosition && (
+          <Marker
+            coordinate={currentPosition}
+            title="My position"
+            pinColor="#fecb2d"
+          />
+        )}
       </MapView>
-
-      <Text> TEST </Text>
-    </View>
+      <BottomSheet />
+    </GestureHandlerRootView>
   );
 }
 
