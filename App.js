c73db3dfa753a1874114cee4277b59a0e50151cb
Ferrieres <benoitferrieres1@gmail.com>
2022-10-31 12:13:50 +0100

    nothing

--- App.js
+++ App.js
@@ -60,9 +60,9 @@ const TabNavigator = () => {
     >
       <Tab.Screen name="Main" component={MainScreen} />
       <Tab.Screen name="Profil" component={ProfilScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="Main" component={MainScreen} />
-      {/* <Tab.Screen name="Messaging" component={MessagingScreen} />       */}
+      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      {/* <Tab.Screen name="Main" component={MainScreen} /> */}
+      <Tab.Screen name="Messaging" component={MessagingScreen} />      
       <Tab.Screen name="Route" component={RouteScreen} />
       <Tab.Screen name="ChatTest" component={ChatScreenTest} />
     </Tab.Navigator>
@@ -74,17 +74,17 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          <Stack.Screen name="Home" component={HomeScreen} />
           {/* <Stack.Screen name="TabNavigator" component={TabNavigator} /> */}
           {/* <Stack.Screen name = "MiseEnRelation" component= {MiseEnRelation} /> */}
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="SignIn" component={SignInScreen} />
+          {/* decommenter la ligne 78 après fin des test ligne 70 */}
           <Stack.Screen name="SignUp" component={SignUpScreen} />
           {/* <Stack.Screen name="Chat" component={ChatScreen} /> */}
 
-          <Stack.Screen name="Facebook" component={HomeScreen} />
           <Stack.Screen name="Google" component={HomeScreen} />
-          {/* decommenter la ligne 78 après fin des test ligne 70 */}
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          <Stack.Screen name="Facebook" component={HomeScreen} />
           <Stack.Screen name="profil" component={ProfilScreen} />
         </Stack.Navigator>
       </NavigationContainer>
